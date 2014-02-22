1b4084d60c37afccbc378b64c894a6daa2d29eaf
Saposhiente <Saposhiente@server.fake>
2014-02-21 22:22:30 -0800

    First commit

---
+++ src/com/github/Saposhiente/ClassDefSubstitutor/ClassDefSubstitutor.java
@@ -0,0 +1,134 @@
+package com.github.Saposhiente.ClassDefSubstitutor;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Saposhiente
+ */
+public class ClassDefSubstitutor {
+    private final Logger logger;
+
+    public ClassDefSubstitutor(Logger logger) {
+        this.logger = logger;
+    }
+    
+    private static final Method defineClass; //probably going to call it a lot, cache it.
+    static {
+        try {
+            defineClass = ClassLoader.class.getDeclaredMethod("defineClass", new Class[]{String.class, byte[].class, int.class, int.class});
+            defineClass.setAccessible(true);
+        } catch (NoSuchMethodException ex) {
+            throw new RuntimeException("Could not load class definer: ", ex);
+        }
+    }
+    public static Class loadClass(String className, byte[] bytecode) {
+        try {
+            return (Class) defineClass.invoke(ClassLoader.getSystemClassLoader(), new Object[]{className, bytecode, 0, bytecode.length});
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            throw new RuntimeException("Could not load class " + className + ": ", ex);
+        }
+    }
+    /**
+     * Start of the template for a blank class to use in class substitution.
+     */
+    static final byte[] classTemplateStartBytes = new byte[]{-54, -2, -70, -66, 0, 0, 0, 51, 0, 5, 1}; //todo: remove last zero and increase max classname size to 65536
+    /**
+     * End of the template for a blank class to use in class substitution.
+     * @see #classTemplateStartBytes
+     */
+    static final byte[] classTemplateEndBytes = new byte[]{7, 0, 1, 1, 0, 16, 106, 97, 118, 97, 47, 108, 97, 110, 103, 47, 79, 98, 106, 101, 99, 116, 7, 0, 3, 0, 33, 0, 2, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0};
+    /**
+     * Creates a the bytecode for a blank class with the specified name
+     * @param name The name of the class
+     * @see #classTemplateStartBytes
+     */
+    public static byte[] createBytecodeFromTemplate(final String name) {
+        final byte[] nameBytes = name.getBytes(Charset.forName("UTF8"));
+        if (nameBytes.length > 255) {
+            throw new IllegalArgumentException("Can't handle class names longer than 255 bytes! (" + name + ")");
+        }
+        final byte[] bytes = new byte[classTemplateStartBytes.length + 2 + nameBytes.length + classTemplateEndBytes.length];
+        System.arraycopy(classTemplateStartBytes, 0, bytes, 0, classTemplateStartBytes.length);
+        bytes[classTemplateStartBytes.length] = (byte) (nameBytes.length >>> 8); 
+        bytes[classTemplateStartBytes.length + 1] = (byte) nameBytes.length; 
+        System.arraycopy(nameBytes, 0, bytes, classTemplateStartBytes.length + 2, nameBytes.length);
+        System.arraycopy(classTemplateEndBytes, 0, bytes, classTemplateStartBytes.length + nameBytes.length + 2, classTemplateEndBytes.length);
+        return bytes;
+    }
+    /**
+     * Creates a new, blank class with the missing class's name and package
+     * 
+     * @param error The NoClassDefFoundError produced because of the missing class
+     */
+    public void substituteClassDef(NoClassDefFoundError error) {
+        substituteClassDef(error, logger);
+    }
+    public static void substituteClassDef(NoClassDefFoundError error, Logger logger) {
+        final String target = error.toString();
+        final String targetBytecodeName;
+        if (target.endsWith(";")) {
+            /*final String targetInternalName = target.substring(target.lastIndexOf(" ") + 1, target.length()-1); Old version code
+            targetBytecodeName = targetInternalName.substring(1);*/
+            targetBytecodeName = target.substring(target.lastIndexOf(" ") + 2, target.length() - 1);
+        } else {
+            targetBytecodeName = target.substring(target.lastIndexOf(" ") + 1);
+        }
+        /*final String targetClassFullName = targetInternalName.replace('/', '.');
+        final String targetClassName = targetClassFullName.substring(1);*/
+        final String targetClassName = targetBytecodeName.replace('/', '.');
+        logger.log(Level.INFO, "Creating substitution class {0}", targetBytecodeName);
+        loadClass(targetClassName, createBytecodeFromTemplate(targetBytecodeName));
+    }
+    public void substituteClassDefs(Class clazz) {
+        substituteClassDefs(clazz, logger);
+    }
+    public static void substituteClassDefs(Class clazz, Logger logger) {
+        substituteFieldDefs(clazz, 0, logger);
+        substituteMethodDefs(clazz, 0, logger); //do one at a time so that getFields() isn't called repeatedly once it begins to succeed
+        substituteConstructorDefs(clazz, 0, logger);
+        if (clazz.getSuperclass() != null) {
+            substituteClassDefs(clazz.getSuperclass(), logger);
+        }
+    }
+    public static void substituteFieldDefs(Class clazz, int recursions, Logger logger) {
+        try {
+            clazz.getDeclaredFields(); 
+        } catch (NoClassDefFoundError ex) {
+            substituteClassDef(ex, logger);
+            recursions++;
+            if (recursions > 100) {
+                throw new RuntimeException("Too many class def substitutions for fields within class " + clazz.getSimpleName() + "! Aborting substitution process.");
+            }
+            substituteFieldDefs(clazz, recursions, logger);
+        }
+    }
+    public static void substituteMethodDefs(Class clazz, int recursions, Logger logger) {
+        try {
+            clazz.getDeclaredMethods(); 
+        } catch (NoClassDefFoundError ex) {
+            substituteClassDef(ex, logger);
+            recursions++;
+            if (recursions > 100) {
+                throw new RuntimeException("Too many class def substitutions for methods within class " + clazz.getSimpleName() + "! Aborting substitution process.");
+            }
+            substituteMethodDefs(clazz, recursions, logger);
+        }
+    }
+    public static void substituteConstructorDefs(Class clazz, int recursions, Logger logger) {
+        try {
+            clazz.getDeclaredConstructors(); 
+        } catch (NoClassDefFoundError ex) {
+            substituteClassDef(ex, logger);
+            recursions++;
+            if (recursions > 100) {
+                throw new RuntimeException("Too many method def substitutions for constructors within class " + clazz.getSimpleName() + "! Aborting substitution process.");
+            }
+            substituteConstructorDefs(clazz, recursions, logger);
+        }
+    }
+}
---
+++ src/com/github/Saposhiente/ClassDefSubstitutor/SubstitutorTester.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.Saposhiente.ClassDefSubstitutor;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * With ASM, can generate the bytecode required to update ClassDefSubstitutor, if a Java update breaks it.
+ * @author Saposhiente
+ */
+public class SubstitutorTester {
+    static final Logger logger = Logger.getLogger("SubstitutorTester");
+    public static void printBytes(byte[] bytes, Logger logger) {
+        StringBuilder builder = new StringBuilder("{");
+        int last = bytes.length - 1;
+        for (int i = 0; i < last; i++) {
+            builder.append(bytes[i]).append(", ");
+        }
+        builder.append(bytes[last]).append("};");
+        logger.info(builder.toString());
+    }
+    public static void main(String[] args) throws IOException {//testing method
+        ClassDefSubstitutor instance = new ClassDefSubstitutor(logger);
+        /*//net.cerberusstudios.llama.runecraft.Runecraft.class.getDeclaredFields() throws NoClassDefFoundError
+        instance.substituteClassDefs(net.cerberusstudios.llama.runecraft.Runecraft.class);
+        net.cerberusstudios.llama.runecraft.Runecraft.class.getDeclaredFields(); //now works*/
+        byte[][] surroundingBytes = getClassTemplate();
+        printBytes(surroundingBytes[0], logger);
+        printBytes(surroundingBytes[1], logger);
+        logger.log(Level.INFO, "{0}", java.util.Arrays.equals(surroundingBytes[0], ClassDefSubstitutor.classTemplateStartBytes));
+        logger.log(Level.INFO, "{0}", java.util.Arrays.equals(surroundingBytes[1], ClassDefSubstitutor.classTemplateEndBytes));
+    }
+    //Use the code below to generate the correct values for classTemplateStartBytes and classTemplateEndBytes
+    public static final String randomInternalName = "LYgltwEOOED/gNQrfssiDjSVJuTKg/ZHjrZuDPbkurpigGquM/ToHaWxebTukXoAU"; //random class name inputted and searched for by getClassTemplate()
+    public static byte[][] getClassTemplate() throws IOException { //requires ASM, produces the correct values for classTemplateStartBytes and classTemplateEndBytes
+        final String targetBytecodeName = randomInternalName.substring(1);
+        final String targetClassFullName = randomInternalName.replace('/', '.');//todo: maybe have to manually pattern match
+        final String targetClassName = targetClassFullName.substring(1);
+        final org.objectweb.asm.ClassWriter writer = new org.objectweb.asm.ClassWriter(0);
+        //writer.visit(51, 33, targetBytecodeName, null, "java/lang/Object", new String[]{});//may break with Java updates, use log below for correct numbers
+        //writer.visitEnd(); 
+        if (substReader == null) {
+            loadSubstReader();
+        }
+        final org.objectweb.asm.ClassVisitor renamer = new org.objectweb.asm.ClassVisitor(org.objectweb.asm.Opcodes.ASM4, null) {
+            @Override
+            public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+                logger.log(Level.INFO, "visit({0}, {1}, targetBytecodeName, {2}, {3}, new String[]{})", new Object[]{version, access, signature, superName});
+                writer.visit(version, access, targetBytecodeName, signature, superName, interfaces);
+            }
+            @Override
+            public void visitEnd() {
+                writer.visitEnd();
+            }
+            
+        };
+        substReader.accept(renamer, org.objectweb.asm.ClassReader.SKIP_DEBUG + org.objectweb.asm.ClassReader.SKIP_FRAMES);
+        final byte[] bytes = writer.toByteArray();
+        logger.info("Testing...");
+        ClassDefSubstitutor.loadClass(targetClassName, bytes);
+        logger.info("Test succeeded.");
+        final byte[] searchBytes = targetBytecodeName.getBytes(Charset.forName("UTF8"));
+        int foundPos = -1;
+        outer:
+        for (int i = 0; i < bytes.length; i++) { // loop over source array
+            for (int j = 0; ; j++) { // check if target matches source from element i
+                if (bytes[i + j] != searchBytes[j]) {
+                    break;
+                }
+                if (j >= 63) { //length is always 64
+                    foundPos = i;
+                    break outer;
+                }
+            }
+        }
+        if (foundPos == -1) {
+            logger.info(new String(bytes));
+            throw new RuntimeException("Did not find bytes!");
+        }
+        final byte[] startBytes = new byte[foundPos - 2]; //skip the length number
+        int endPos = foundPos + 64;
+        final int endLength = bytes.length - endPos;
+        final byte[] endBytes = new byte[endLength];
+        System.arraycopy(bytes, 0, startBytes, 0, foundPos - 2);
+        System.arraycopy(bytes, endPos, endBytes, 0, endLength);
+        return new byte[][]{startBytes, endBytes};
+    }
+    static org.objectweb.asm.ClassReader substReader = null;
+    public static void loadSubstReader() throws IOException {
+        substReader = new org.objectweb.asm.ClassReader(SubstitutorTester.class.getResourceAsStream("/com/github/Saposhiente/ClassDefSubstitutor/SubstituteClass.class"));
+        //substReader = new org.objectweb.asm.ClassReader(new java.io.FileInputStream("/path/to/SubstituteClass.class"));// SubstituteClass.class source code: public class A {}
+    }
+}
